9dc0309fa6411a494f0de588399a370984f9158f
OsmanFP <osmanwako411@gmail.com>
2023-05-05 12:01:45 +0300

    Create logic folder and use calculate.js and operate.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,37 +1,172 @@
-import Button from './Button';
-import { setTerm, reduceterm } from './operation';
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 
-function Calculator() {
-  const createterm = (str) => {
-    let temp = str[0];
-    const operator = '*+-%÷';
-    let term = [];
-    let i = 1;
-    while (i < str.length) {
-      if (str[i] === '+') {
-        term = [...term, temp, str[i]];
-        temp = '';
-      } else if (str[i] === '-' && !operator.includes(str[i - 1])) {
-        term = [...term, temp, str[i]];
-        temp = '';
-      } else {
-        temp = `${temp}${str[i]}`;
-      }
-      i += 1;
-    }
-    return [...term, temp];
-  };
-  const setanswer = (temp) => {
-    let answer = 'Error';
-    const lists = createterm(temp);
-    // console.log(temp, lists);
-    const terms = lists.map((list) => (list.includes('%') || list.includes('x') || list.includes('÷')
-      ? setTerm(list)
-      : list));
-    answer = reduceterm(terms);
-    return answer;
-  };
-  return <Button getanswer={setanswer} />;
-}
+const Calculator = () => {
+  const [calcState, setCalcState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
+  const displayResult = (e) => {
+    const name = e.target.textContent;
+    setCalcState((prev) => ({
+      ...prev,
+      total: name,
+    }));
+  };
+
+  const onbuttonclick = (e) => {
+    const keyValue = e.target.textContent;
+    const result = calculate(calcState, keyValue);
+    setCalcState(result);
+  };
+  const { total, next, operation } = calcState;
+  return (
+    <div className="grid-calculator">
+      <button
+        type="button"
+        className="input button items"
+        onChange={displayResult}
+      >
+        {total}
+        {operation}
+        {next}
+      </button>
+      <button
+        type="button"
+        className="clear button items"
+        onClick={onbuttonclick}
+      >
+        AC
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="sign button items"
+      >
+        +/-
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="module button operator"
+      >
+        %
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="divided operator button"
+      >
+        ÷
+      </button>
+      <button
+        type="button"
+        className="seven button number"
+        onClick={onbuttonclick}
+      >
+        7
+      </button>
+      <button
+        type="button"
+        className="eight button number"
+        onClick={onbuttonclick}
+      >
+        8
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="nine button number"
+      >
+        9
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="times button operator"
+      >
+        x
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="four button number"
+      >
+        4
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="five button number"
+      >
+        5
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="six button number"
+      >
+        6
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="minus operator button"
+      >
+        -
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="three button number"
+      >
+        3
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="two button number"
+      >
+        2
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="one button number"
+      >
+        1
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="plus operator button"
+      >
+        +
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="zero button number"
+      >
+        0
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="dot button items"
+      >
+        .
+      </button>
+      <button
+        type="button"
+        onClick={onbuttonclick}
+        className="equal button items"
+      >
+        =
+      </button>
+    </div>
+  );
+};
 export default Calculator;
