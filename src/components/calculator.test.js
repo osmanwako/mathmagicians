8eea1c8f8cb5f58b15eacff951c9fb7d626d93ce
Osman Wako <osmanwako411@gmail.com>
2023-05-12 04:50:41 +0300

    Update calculator.test.js

--- src/components/calculator.test.js
+++ src/components/calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Calculator from './Calculator';
+import Calculator from './calculator';
 
 test('Calculator component renders correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
