ad1e4736d77fb0c2ca64bc3e32b0edf2174304a0
OsmanFP <osmanwako411@gmail.com>
2023-05-12 07:32:26 +0300

    Fix deploy error

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,13 @@ function Header() {
       <h1 className="nav-logo">Math Magicians </h1>
       <ul className="nav-unorder">
         <li className="nav-list">
-          <Link to="/" className="nav-link">Home</Link>
+          <Link to="./" className="nav-link">Home</Link>
         </li>
         <li>
-          <Link to="/calculator" className="nav-link">Calculator</Link>
+          <Link to="./calculator" className="nav-link">Calculator</Link>
         </li>
         <li>
-          <Link to="/quotes" className="nav-link link-quote">Quotes</Link>
+          <Link to="./quotes" className="nav-link link-quote">Quotes</Link>
         </li>
       </ul>
     </nav>
