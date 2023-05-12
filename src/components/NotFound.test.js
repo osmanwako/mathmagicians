322bf6289f011db45f19e2afd8b5ebb2db9743e3
OsmanFP <osmanwako411@gmail.com>
2023-05-12 04:20:07 +0300

    Create test for all components

---
+++ src/components/NotFound.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NotFound from './NotFound';
+
+test('NotFound component renders correctly', () => {
+    const tree = renderer.create(<NotFound />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
