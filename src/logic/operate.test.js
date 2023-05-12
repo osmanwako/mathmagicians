83c7d2e37fcb352c8ad94bd5e79a926890cbb609
OsmanFP <osmanwako411@gmail.com>
2023-05-12 06:16:54 +0300

    Add test for AC,/ and module in both file

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -5,11 +5,27 @@ describe('Testing the operate component:', () => {
     expect(operate(45, 55, '+')).toBe('100');
   });
 
-  it('2. Testing divide by zero error:', () => {
+  it('2. Testing Multiplication:', () => {
+    expect(operate(8, 8, 'x')).toBe('64');
+  });
+
+  it('3. Testing Substraction:', () => {
+    expect(operate(45, 55, '-')).toBe('-10');
+  });
+
+  it('4. Testing Division:', () => {
+    expect(operate(20, 5, '÷')).toBe('4');
+  });
+
+  it('5. Testing Division:', () => {
+    expect(operate(9, 5, '%')).toBe('4');
+  });
+
+  it('6. Testing divide by zero error:', () => {
     expect(operate(1, 0, '÷')).toBe("Can't divide by 0.");
   });
 
-  it('3. Test not an operation error:', () => {
+  it('7. Test not an operation error:', () => {
     expect(() => { operate(99, 0, '*'); }).toThrow(Error);
   });
 });
