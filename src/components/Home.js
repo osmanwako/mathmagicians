cced15d7aa245d920685f2aa2223af092b05f0f5
OsmanW <osmanwako411@gmail.com>
2023-10-12 16:05:19 +0300

    Update Quote

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,14 @@
-function getMessage() {
-  const str = `Sure! Magic Maths is a set of React components that allow you to create interactive and animated visualizations with declarative code. It is a great tool for creating educational content, games, and simulations that involve mathematical concepts. You can use it to create graphs, charts, animations, and more. The components are easy to use and customizable, and they come with a wide range of features such as zooming, panning, and animation controls`;
-  return str;
-}
-
 function Home() {
   return (
     <div>
       <h3>Welcome to Our page</h3>
       <p>
-        {getMessage()}
-      </p>
-      <p>
-        {getMessage()}
+        Sure! Magic Maths is a set of React components that allow you to create interactive
+        and animated visualizations with declarative code. It is a great tool for creating
+        educational content, games, and simulations that involve mathematical concepts.
+        You can use it to create graphs,charts, animations, and more.
+        The components are easy to use and customizable, and they come with a
+        wide range of features such as zooming, panning, and animation controls
       </p>
     </div>
   );
--- src/components/government.js
+++ src/components/government.js
@@ -1,12 +1,18 @@
 function Government() {
-  return <div className="desc-message">
-    <q>
-    For nearly four years you have had an Administration which instead of twirling its thumbs has rolled up its sleeves.
-    We will keep our sleeves rolled up. We had to struggle with the old enemies of peace–business and financial monopoly,
-    speculation, reckless banking,
-    class antagonism, sectionalism, war profiteering </q> 
-    -<b> Franklin D.Roosevelt</b>
-    </div>;
+  return (
+    <div className="desc-message">
+      <q>
+        For nearly four years you have had an Administration which
+        instead of twirling its thumbs has rolled up its sleeves.
+        We will keep our sleeves rolled up.
+        We had to struggle with the old enemies of peace–business
+        and financial monopoly,speculation, reckless banking,
+        class antagonism, sectionalism, war profiteering
+      </q>
+      -
+      <b> Franklin D.Roosevelt</b>
+    </div>
+  );
 }
 
 export default Government;
